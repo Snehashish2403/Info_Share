233b32729ca77834fc06206f69fa9904f44104ee
Mriganka Bharali <mrigankabharali30@gmail.com>
2021-01-14 02:48:14 +0530

    Profile Layout

--- app/src/main/java/com/crazydevstuff/infoshare/Fragments/ProfileFragment.java
+++ app/src/main/java/com/crazydevstuff/infoshare/Fragments/ProfileFragment.java
@@ -1,66 +1,95 @@
 package com.crazydevstuff.infoshare.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.crazydevstuff.infoshare.Activities.Login;
+import com.crazydevstuff.infoshare.Models.RegisterModel;
 import com.crazydevstuff.infoshare.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import static android.content.ContentValues.TAG;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView profileNameTV, profileNumberTV, profileEmailTV;
+    private ImageView profileImageIV;
+    private FirebaseAuth firebaseAuth;
+    private Button logOutButton;
+    private DatabaseReference reference;
+    private RegisterModel model;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profileEmailTV = v.findViewById(R.id.user_profileEmailTV);
+        profileImageIV = v.findViewById(R.id.user_profileIV);
+        profileNameTV = v.findViewById(R.id.user_profileNameTV);
+        profileNumberTV = v.findViewById(R.id.user_profileNumberTV);
+        logOutButton = v.findViewById(R.id.logOutButton);
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOut();
+            }
+        });
+
+        setInfo();
+
+        return v;
+    }
+    private void logOut(){
+        firebaseAuth.signOut();
+        startActivity(new Intent(getContext(),Login.class));
+        getActivity().finish();
+    }
+    private void setInfo(){
+        String item="https://srmrc.nihr.ac.uk/wp-content/uploads/female-placeholder.jpg";
+        Picasso.get().load(item)
+                .fit()
+                .centerInside()
+                .into(profileImageIV);
+        profileEmailTV.setText(firebaseAuth.getCurrentUser().getEmail());
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                model = snapshot.getValue(RegisterModel.class);
+                System.out.println(model.getName() + model.getEmail());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "loadPost:onCancelled", error.toException());
+            }
+        });
     }
 }
